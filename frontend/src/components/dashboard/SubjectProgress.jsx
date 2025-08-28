f70f3ccbd04fa93a77da624df76a2687fe4f8d85
Achyuth <achyuthkumar2001@gmail.com>
2025-08-28 12:22:23 -0500

    updated to signup validation and subject deletion

--- frontend/src/components/dashboard/SubjectProgress.jsx
+++ frontend/src/components/dashboard/SubjectProgress.jsx
@@ -27,7 +27,7 @@ const SubjectProgress = ({ subjects }) => {
               key={index}
               subject={subject.name} 
               percentage={subject.progress} 
-              topics={subject.topics.length}
+              topics={subject.topics?.length || 0}
             />
           ))
         ) : (
